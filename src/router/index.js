352bc28f75ba33e9eb6d032a198d75113347a1c9
Locke6 <1031629291@qq.com>
2020-11-28 13:01:27 +0800

    feat:重写$router.push/replace

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,25 @@ import Search from "../views/Search";
 import Login from "../views/Login";
 import Register from "../views/Register";
 Vue.use(VueRouter);
+
+//重写$router的push方法,因为原生push会返回promise，如果重复调用push会报错
+const push = VueRouter.prototype.push
+VueRouter.prototype.push = function (location, onComplete, onAbort) {
+  if (location && onComplete && onAbort) {
+    return push.call(this, location, onComplete, onAbort)
+  }
+  return push.call(this, location, onComplete, () => { })
+}
+
+//重写$router的replace方法
+const replace = VueRouter.prototype.replace
+Vue.prototype.replace = function (location, onComplete, onAbort) {
+  if (location && onComplete && onAbort) {
+    return replace.call(this, location, onComplete, onAbort)
+  }
+  return replace.call(this, location, onComplete, () => { })
+}
+
 export default new VueRouter({
   routes: [
     {
